1d5b0b01e7269ecefa38d1179f3f0ce573130021
infobarbosa <infobarbosa@yahoo.com.br>
2025-09-07 16:23:01 -0300

    Refactor Cloud9 environment setup script to use a specific Ubuntu 22.04 image and updated instance type

--- assets/scripts/lab-data-eng-cloud9-environment.sh
+++ assets/scripts/lab-data-eng-cloud9-environment.sh
@@ -1,21 +1,11 @@
+#! /bin/bash
 
-# Listar as imagens disponíveis
-aws ec2 describe-images --output json --region us-east-1 \
---filters "Name=name,Values=ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server*" \
---query 'sort_by(Images, &CreationDate)[-1].{Name: Name, ImageId: ImageId, CreationDate: CreationDate, Owner:OwnerId}'
-
-# Selecionar a última imagem disponível
-export IMAGE_ID=$(aws ec2 describe-images --output json --region us-east-1 \
---filters "Name=name,Values=ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server*" \
---query 'sort_by(Images, &CreationDate)[-1].{ImageId: ImageId}' | jq -r '.ImageId')
-
-# Criar o ambiente cloud9
 aws cloud9 create-environment-ec2 \
-    --name lab-data-eng \
-    --description "Ambiente cloud9 para o bootcamp de advanced data engineering." \
-    --instance-type t2.micro \
+    --name data-eng-aula-$(date +%Y-%m-%d-%H-%M) \
+    --description "Ambiente Cloud9 com Ubuntu 22.04 e m5.large para engenharia de dados." \
+    --instance-type m5.large \
+    --image-id ubuntu-22.04-x86_64 \
     --automatic-stop-time-minutes 60 \
-    --connection-type CONNECT_SSH \
-    --image-id $IMAGE_ID
+    --connection-type CONNECT_SSH
 
 
