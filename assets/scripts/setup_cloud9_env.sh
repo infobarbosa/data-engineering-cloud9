11f20c04cc004ec31ef45be5bc2040ff0a89958b
infobarbosa <infobarbosa@yahoo.com.br>
2026-01-21 08:05:51 -0300

    fix: make apt upgrade non-interactive with forced configuration options and disable apt update.

--- assets/scripts/setup_cloud9_env.sh
+++ assets/scripts/setup_cloud9_env.sh
@@ -1,5 +1,6 @@
 echo "### Atualizando o sistema ###"
-sudo apt update -y && sudo apt upgrade -y
+# sudo apt update -y && sudo apt upgrade -y
+sudo DEBIAN_FRONTEND=noninteractive apt upgrade -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"
 
 echo "### Instalando o pacote boto3  ###"
 pip install boto3
